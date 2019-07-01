24d554503da5dc5af113ccb39b375c9f90ef7a62
yangbin1 <yangbin199191@163.com>
2019-07-01 09:51:43 +0800

    Initial commit

---
+++ src/main/java/com/example/bootredis/schedule/ClusterLockV2Job.java
@@ -0,0 +1,18 @@
+package com.example.bootredis.schedule;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName: ClusterLockV2Job
+ * @Auther: yangbin1
+ * @Date: 2019/6/29 22:01
+ * @Description:
+ */
+@Service
+public class ClusterLockV2Job {
+
+//    @Scheduled(cron = "0/5 * * * * *")
+//    public void lock() {
+//        System.out.println("enter job" + System.currentTimeMillis());
+//    }
+}

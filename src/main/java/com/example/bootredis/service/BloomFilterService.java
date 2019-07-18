df5dc02f549cc4690b1c9a3296065a67ae75d2a8
yangbin1 <yangbin199191@163.com>
2019-07-18 21:42:43 +0800

    2

---
+++ src/main/java/com/example/bootredis/service/BloomFilterService.java
@@ -0,0 +1,14 @@
+package com.example.bootredis.service;
+
+import javax.annotation.PostConstruct;
+
+public class BloomFilterService {
+
+    /**
+     * 程序启动时加载此方法
+     */
+    @PostConstruct
+    public void initBloomFilter(){
+
+    }
+}
